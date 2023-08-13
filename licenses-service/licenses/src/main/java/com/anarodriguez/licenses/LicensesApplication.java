041702d42a029ea1f4f4f33c75e46f21b0155753
Ana Rodriguez Fernandez <ana.rodriguez.fernandez.87@gmail.com>
2023-08-13 18:10:35 +0200

    Remove Embedded dependencies from Spring App

--- licenses-service/licenses/src/main/java/com/anarodriguez/licenses/LicensesApplication.java
+++ licenses-service/licenses/src/main/java/com/anarodriguez/licenses/LicensesApplication.java
@@ -1,10 +1,9 @@
 package com.anarodriguez.licenses;
 
-import de.flapdoodle.embed.mongo.spring.autoconfigure.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = EmbeddedMongoAutoConfiguration.class)
+@SpringBootApplication
 public class LicensesApplication {
 
     public static void main(String[] args) {
