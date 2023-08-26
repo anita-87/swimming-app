5b9ad53058f86662b6d544cc025d127a738a8465
Ana Rodriguez Fernandez <ana.rodriguez.fernandez.87@gmail.com>
2023-08-26 18:43:37 +0200

    Update profile services

--- frontend/src/app/profiles/profiles.component.ts
+++ frontend/src/app/profiles/profiles.component.ts
@@ -1,7 +1,7 @@
 import {Component, ViewChild} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {MatTableDataSource} from "@angular/material/table";
-import {MatPaginator} from "@angular/material/paginator";
+import {MatPaginator, PageEvent} from "@angular/material/paginator";
 import {Profile} from "../models/profile";
 import {ProfilesService} from "../services/profiles.service";
 
@@ -13,12 +13,15 @@ import {ProfilesService} from "../services/profiles.service";
 export class ProfilesComponent {
 
   displayedColumns: string[] = ['firstName', 'lastName', 'dateOfBirth'];
+  profiles: Profile[];
   dataSource: MatTableDataSource<Profile>;
+  numOfProfiles = 0
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private router: Router, private activatedRoute: ActivatedRoute, private profilesService: ProfilesService) {
     this.dataSource = new MatTableDataSource<Profile>();
+    this.profiles = [];
   }
 
   ngOnInit(): void {
@@ -27,11 +30,21 @@ export class ProfilesComponent {
 
   getSwimProfiles(): void {
     this.profilesService.getSwimmers()
-      .subscribe(profiles =>this.dataSource =  new MatTableDataSource<Profile>(profiles))
+      .subscribe(profiles => {
+        this.profiles = profiles;
+        this.dataSource =  new MatTableDataSource<Profile>(this.profiles.slice(0, 10));
+        this.numOfProfiles = profiles.length;
+      })
   }
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+  }
+
+  handlePageEvent(e: PageEvent) {
+    let startIndex = e.pageIndex * e.pageSize
+    let endIndex = e.pageIndex * e.pageSize + e.pageSize
+    this.dataSource = new MatTableDataSource<Profile>(this.profiles.slice(startIndex, endIndex));
   }
 
   applyFilter(event: Event) {
--- frontend/src/app/services/profiles.service.ts
+++ frontend/src/app/services/profiles.service.ts
@@ -2,21 +2,22 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {catchError, Observable, of, tap} from "rxjs";
 import {Profile} from "../models/profile";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfilesService {
 
-  private swimmersUrl = 'http://localhost:8080/profiles/swim'
+  private swimmersUrl = "/profiles/swim"
 
   constructor(private http: HttpClient) { }
 
   getSwimmers(): Observable<Profile[]> {
-    return this.http.get<Profile[]>(this.swimmersUrl)
+    return this.http.get<Profile[]>(`${environment.API_URL}${this.swimmersUrl}`)
       .pipe(
         tap(_ => console.log('swim profiles fetched')),
-        catchError(this.handleError<Profile[]>('getHeroes', []))
+        catchError(this.handleError<Profile[]>('getSwimmers', []))
       )
   }
 
@@ -24,7 +25,6 @@ export class ProfilesService {
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
 
-      // TODO: better job of transforming error for user consumption
       console.error(`${operation} failed: ${error.message}`);
 
       // Let the app keep running by returning an empty result.
